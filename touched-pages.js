4dc2542d615b808f2c1ecf31e7c59abfe4b42a10
Alexander I. Zaytsev <hazzik@gmail.com>
2013-02-12 00:20:03 +1300

    Update touched-pages.js
    Code reformat

--- touched-pages.js
+++ touched-pages.js
@@ -2,80 +2,60 @@
     (function ($) {
         $(function () {
             $('html').addClass('touched-pages-user-js');
-    
-            var link = $('<a>').attr('href', '#').html('Затронутые&nbsp;страницы');
-            var menu = $('<li id="touched-pages"></li>').append(link);
-            $('li#pt-mycontris').after(menu);
-    
-            $('#touched-pages a', 'ul').click(function(){
-                var links = [];
-    
-                function parseData(data, links) {
-                   $(data).each(function(){
-                        if(!links[this.title])
-                        {
-                            var a = $('<a>').attr('href', '/wk/' + this.title).attr('target', '_blank').html(this.title);
-                            $('div#content').append(a).append('<br />');
-                            links[this.title] = true;
-                        }
-                    });
-                }
-    
-                function loadAdditionalDataIfNeeded(data, links)
-                {
-                    if (data['query-continue'] && data['query-continue']['usercontribs']['ucstart'] > 1 && confirm('Показаны не все результаты. Добрать недостающие?'))
-                    {
-                        $.get(
-                            '/api.php',
-                            {
-                                format: 'json',
-                                list: 'usercontribs',
-                                action: 'query',
-                                uclimit: 500,
-                                ucdir: 'newer',
-                                ucuser: wgUserName,
-                                ucstart: data['query-continue']['usercontribs']['ucstart']
-                            },
-                            function(data)
-                            {
-                                if (data.query)
-                                {
-                                    parseData(data.query.usercontribs, links);
-    
-                                    loadAdditionalDataIfNeeded(data, links);
-    
-                                    $('div#content').prepend('Показано '+ $('a', 'div#content').length +' затронутых страниц.<br /><br />');
-                                }
-                            },
-                            'json'
-                        );
+                   
+    	    function parseData(data, links) {
+                $(data).each(function () {
+                    if(!links[this.title]) {
+                        var a = $('<a>').attr('href', '/wk/' + this.title).attr('target', '_blank').html(this.title);
+                        $('div#content').append(a).append('<br />');
+                        links[this.title] = true;
                     }
+                });
+            }
+			
+            function loadAdditionalDataIfNeeded(data, links) {
+                if(data['query-continue'] && data['query-continue']['usercontribs']['ucstart'] > 1 && confirm('Показаны не все результаты. Добрать недостающие?')) {
+                    $.get('/api.php', {
+                        format: 'json',
+                        list: 'usercontribs',
+                        action: 'query',
+                        uclimit: 500,
+                        ucdir: 'newer',
+                        ucuser: wgUserName,
+                        ucstart: data['query-continue']['usercontribs']['ucstart']
+                    }, function (data) {
+                        if(data.query) {
+                            parseData(data.query.usercontribs, links);
+                            loadAdditionalDataIfNeeded(data, links);
+                            $('div#content').prepend('Показано ' + $('a', 'div#content').length + ' затронутых страниц.<br /><br />');
+                        }
+                    }, 'json');
                 }
-    
-                $('#p-cactions').hide();
-                $('div#content').html('');
-                $.get(
-                    '/api.php',
-                    {
+            }
+
+            var link = $('<a>')
+                .attr('href', '#')
+                .html('Затронутые&nbsp;страницы')
+                .insertAfter('li#pt-mycontris')
+                .wrap('<li id="touched-pages"></li>')
+                .click(function () {
+                    var links = [];
+                    $('#p-cactions').hide();
+                    $('div#content').html('');
+                    $.get('/api.php', {
                         format: 'json',
                         list: 'usercontribs',
                         action: 'query',
                         uclimit: 500,
                         ucdir: 'newer',
                         ucuser: wgUserName
-                    },
-                    function(data)
-                    {
-                        if (data.query)
-                        {
+                    }, function (data) {
+                        if(data.query) {
                             parseData(data.query.usercontribs, links);
                             loadAdditionalDataIfNeeded(data, links);
                         }
-                    },
-                    'json'
-                );
-                return false;
-            });
+                    }, 'json');
+                    return false;
+                });
         });
     })(jQuery);
-    //</source>
