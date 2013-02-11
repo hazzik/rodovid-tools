f9089d0a2da9faa79b7426272eb6dc168a45f11f
Alexander I. Zaytsev <hazzik@gmail.com>
2013-02-12 00:32:22 +1300

    Update touched-pages.js

--- touched-pages.js
+++ touched-pages.js
@@ -1,11 +1,31 @@
     //<source lang="javascript">
     (function ($, window) {
+    	function showData () {
+            var links = [];
+            $('#p-cactions').hide();
+            $('div#content').html('');
+            $.get('/api.php', {
+                format: 'json',
+                list: 'usercontribs',
+                action: 'query',
+                uclimit: 500,
+                ucdir: 'newer',
+                ucuser: wgUserName
+            }, function (data) {
+                if(data.query) {
+                    parseData(data.query.usercontribs, links);
+                    loadAdditionalDataIfNeeded(data, links);
+                }
+            }, 'json');
+            return false;
+        }
+        
     	function parseData(data, links) {
     		$(data).each(function () {
         		if(!links[this.title]) {
             		var a = $('<a>').attr('href', '/wk/' + this.title).attr('target', '_blank').html(this.title);
             		$('div#content').append(a).append('<br />');
-            		links[this.title] = true;
+            			links[this.title] = true;
         		}
     		});
     	}
@@ -38,24 +58,6 @@
                 .html('Затронутые&nbsp;страницы')
                 .insertAfter('li#pt-mycontris')
                 .wrap('<li id="touched-pages"></li>')
-                .click(function () {
-                    var links = [];
-                    $('#p-cactions').hide();
-                    $('div#content').html('');
-                    $.get('/api.php', {
-                        format: 'json',
-                        list: 'usercontribs',
-                        action: 'query',
-                        uclimit: 500,
-                        ucdir: 'newer',
-                        ucuser: wgUserName
-                    }, function (data) {
-                        if(data.query) {
-                            parseData(data.query.usercontribs, links);
-                            loadAdditionalDataIfNeeded(data, links);
-                        }
-                    }, 'json');
-                    return false;
-                });
+                .click(showData);
         });
     })(jQuery, window);
